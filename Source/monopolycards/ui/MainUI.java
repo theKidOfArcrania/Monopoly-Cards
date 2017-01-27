c1f32c6835ca13c728afd21eb6f382f5c8241b81
theKidOfArcrania <wanghenry1998@gmail.com>
2017-01-27 10:36:08 -0600

    Stuff

--- Source/monopolycards/ui/MainUI.java
+++ Source/monopolycards/ui/MainUI.java
@@ -6,20 +6,23 @@ import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
 import javafx.scene.SubScene;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class MainUI extends Pane
+public class MainUI extends StackPane
 {
 	public static class Main extends Application
 	{
 		private MainUI root;
 		
+		@Override
 		public void init()
 		{
 			root = new MainUI();
 		}
 		
+		@Override
 		public void start(Stage primaryStage)
 		{
 			Scene s = new Scene(root);
@@ -28,14 +31,30 @@ public class MainUI extends Pane
 			primaryStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
 			primaryStage.show();
 		}
+		
+		public static void main(String[] args)
+		{
+			Application.launch(args);
+		}
 	}
 	
 	private Group tableRoot;
 	
 	public MainUI()
 	{
-		tableRoot = new Group();
-		SubScene tableScene = new SubScene(tableRoot, 0, 0, true, SceneAntialiasing.BALANCED);
+		tableRoot = initTable();
 		
+		SubScene tableScene = new SubScene(tableRoot, 0, 0, true, SceneAntialiasing.BALANCED);
+		tableScene.widthProperty().bind(widthProperty());
+		tableScene.heightProperty().bind(heightProperty());
+		tableScene.setFill(Color.ALICEBLUE);		
+		
+		getChildren().add(tableScene);
+	}
+	
+	private Group initTable()
+	{
+		Group root = new Group();
+		return root;
 	}
 }
